91c9bbcd8eee55cfdc218583d69f0ec928f32dbe
ahiguera <ahiguera@heinsohn.com.co>
2020-02-11 22:25:35 -0500

    Se eliminan imports

--- src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
+++ src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
@@ -1,13 +1,5 @@
 package CursoSelenium.CursoSelenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
-import org.testng.annotations.Test;
-
 public class GoogleSearchTest {
 
 }
