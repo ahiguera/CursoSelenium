1684c5e241f4d05a1fa4dfa5402956f6bedcf3b3
Antonio Jose Higuera <ahiguera@heinsohn.com.co>
2020-02-15 21:26:00 -0500

    Se cre la primera prueba

--- src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
+++ src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
@@ -1,5 +1,33 @@
 package CursoSelenium.CursoSelenium;
 
-public class GoogleSearchTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class GoogleSearchTest {
+	
+	
+	@Test
+	public void buscarEltiempo() {
+		
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("http://www.google.com");
+			
+		WebElement searchInput = driver.findElement(By.name("q"));
+		searchInput.sendKeys("ElTiempo");
+		searchInput.sendKeys(Keys.RETURN);
+		
+		
+		driver.close();
+		
+	}
+	
+	
 }
