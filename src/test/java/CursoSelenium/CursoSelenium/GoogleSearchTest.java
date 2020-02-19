4b871702430555b170bfcb4897bd6ba54db1dee7
Antonio Jose Higuera <ahiguera@heinsohn.com.co>
2020-02-18 19:25:36 -0500

    Se agregan 2 pruebas

--- src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
+++ src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
@@ -30,4 +30,46 @@ public class GoogleSearchTest {
 	}
 	
 	
+	@Test
+	public void buscarMazda() {
+		
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("http://www.google.com");
+			
+		WebElement searchInput = driver.findElement(By.name("q"));
+		searchInput.sendKeys("Mazda");
+		searchInput.sendKeys(Keys.RETURN);
+		
+		
+		driver.close();
+		
+	}
+	
+	
+	@Test
+	public void buscarHeinsohn() {
+		
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("http://www.google.com");
+			
+		WebElement searchInput = driver.findElement(By.name("q"));
+		searchInput.sendKeys("Heinsohn");
+		searchInput.sendKeys(Keys.RETURN);
+		
+		
+		driver.close();
+		
+	}
+	
+
+
+	
 }
