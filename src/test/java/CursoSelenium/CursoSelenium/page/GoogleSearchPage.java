42459c2853903e5520456b8aa446df103cfb8678
Antonio Jose Higuera <ahiguera@heinsohn.com.co>
2020-02-18 19:31:26 -0500

    Se agrega el pom para la pagina de busqueda de google

---
+++ src/test/java/CursoSelenium/CursoSelenium/page/GoogleSearchPage.java
@@ -0,0 +1,14 @@
+package CursoSelenium.CursoSelenium.page;
+
+import org.openqa.selenium.By;
+
+public class GoogleSearchPage {
+	
+	public By txtBuscar = By.name("q");
+	
+	public By btnBuscar = By.name("btnK");
+	
+	public By btnSuerte = By.name("btnI");
+	
+	
+}
