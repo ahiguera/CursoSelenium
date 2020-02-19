f14ed79b2a008ddf46f1907a5aba0f7ec07f10e1
Antonio Jose Higuera <ahiguera@heinsohn.com.co>
2020-02-18 19:50:52 -0500

    Se crea el page object model

--- src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
+++ src/test/java/CursoSelenium/CursoSelenium/GoogleSearchTest.java
@@ -7,69 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import CursoSelenium.CursoSelenium.page.GoogleSearchPage;
+
 public class GoogleSearchTest {
 	
+
+	GoogleSearchPage gsp = new  GoogleSearchPage();
+
 	
 	@Test
 	public void buscarEltiempo() {
-		
-		String projectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		
-		driver.get("http://www.google.com");
-			
-		WebElement searchInput = driver.findElement(By.name("q"));
-		searchInput.sendKeys("ElTiempo");
-		searchInput.sendKeys(Keys.RETURN);
-		
-		
-		driver.close();
-		
+		gsp.buscar("ElTiempo");
 	}
 	
 	
 	@Test
 	public void buscarMazda() {
-		
-		String projectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		
-		driver.get("http://www.google.com");
-			
-		WebElement searchInput = driver.findElement(By.name("q"));
-		searchInput.sendKeys("Mazda");
-		searchInput.sendKeys(Keys.RETURN);
-		
-		
-		driver.close();
-		
+		gsp.buscar("Mazda");		
 	}
 	
 	
 	@Test
 	public void buscarHeinsohn() {
-		
-		String projectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		
-		
-		driver.get("http://www.google.com");
-			
-		WebElement searchInput = driver.findElement(By.name("q"));
-		searchInput.sendKeys("Heinsohn");
-		searchInput.sendKeys(Keys.RETURN);
-		
-		
-		driver.close();
-		
+		gsp.buscar("Heinsohn");				
 	}
-	
-
-
 	
 }
--- src/test/java/CursoSelenium/CursoSelenium/page/GoogleSearchPage.java
+++ src/test/java/CursoSelenium/CursoSelenium/page/GoogleSearchPage.java
@@ -1,6 +1,10 @@
 package CursoSelenium.CursoSelenium.page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSearchPage {
 	
@@ -10,5 +14,24 @@ public class GoogleSearchPage {
 	
 	public By btnSuerte = By.name("btnI");
 	
+	
+	
+	public void buscar(String buscarEnGoogle) {
+		
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath +  "/drivers/chrome/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("http://www.google.com");
+			
+		WebElement searchInput = driver.findElement(txtBuscar);
+		searchInput.sendKeys(buscarEnGoogle);
+		searchInput.sendKeys(Keys.RETURN);
+		
+		
+		driver.close();
+		
+	}
 	
 }
